45326e7e295714243fbb5db1eb46a2ffc17048c8
eqbic <erichquerner@posteo.org>
2020-11-29 12:51:15 +0100

    - added email property to contact

--- CleanCode/Contactz/Contact.cs
+++ CleanCode/Contactz/Contact.cs
@@ -7,12 +7,14 @@ namespace Contactz
         public Guid Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string Email { get; set; }
 
-        public Contact(string firstName, string lastName)
+        public Contact(string firstName, string lastName, string email)
         {
             this.Id = Guid.NewGuid();
             this.FirstName = firstName;
             this.LastName = lastName;
+            this.Email = email;
         }
     }
 }
